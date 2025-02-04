ee87ea92f73e0f43e1d88b325fc00e0b459d5e89
Jose Manuel Mendoza Murillo <jose.manuel.mendoza.murillo@gmail.com>
2025-02-03 22:20:39 -0600

    Tailwind css has been installed

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+// tailwind.config.js
+module.exports = {
+    darkMode: 'class',
+    content: ["./src/**/*.{html,ts}"],
+    purge: [],
+    
+    theme: {
+      extend: {},
+    },
+    variants: {},
+    plugins: [],
+}
